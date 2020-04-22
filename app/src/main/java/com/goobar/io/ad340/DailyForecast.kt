9f07de2ba4cbb37a0aa8382a3aeef972a74fa2aa
Nate Ebel <n8ebel@gmail.com>
2020-04-22 08:02:31 -0700

    Add a list of daily forecast items that are clickable
    Items are observed from a repository and generated randomly on each call to load()
    Items display click feedback in the form of a Toast message that displays the clicked forecast data

---
+++ app/src/main/java/com/goobar/io/ad340/DailyForecast.kt
@@ -0,0 +1,9 @@
+package com.goobar.io.ad340
+
+import java.util.*
+
+data class DailyForecast(
+    val date: Date,
+    val temp: Float,
+    val description: String
+)
--- app/src/main/java/com/goobar/io/ad340/MainActivity.kt
+++ app/src/main/java/com/goobar/io/ad340/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+  private val forecastRepository = ForecastRepository()
 
   // region Setup Methods
 
@@ -23,9 +28,25 @@ class MainActivity : AppCompatActivity() {
       if (zipcode.length != 5) {
         Toast.makeText(this, R.string.zipcode_entry_error, Toast.LENGTH_SHORT).show()
       } else {
-        Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+        forecastRepository.loadForecast(zipcode)
       }
     }
+
+    val dailyForecastList: RecyclerView = findViewById(R.id.dailyForecastList)
+    dailyForecastList.layoutManager = LinearLayoutManager(this)
+    val dailyForecastAdapter = DailyForecastListAdapter() {
+      val msg = getString(R.string.forecast_clicked_format, it.temp, it.description)
+      Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+    dailyForecastList.adapter = dailyForecastAdapter
+
+    // Create the observer which updates the UI in response to forecast updates
+    val weeklyForecastObserver = Observer<List<DailyForecast>> { forecastItems ->
+      // update our list adapter
+      dailyForecastAdapter.submitList(forecastItems)
+    }
+    forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
+
   }
 
   override fun onStart() {

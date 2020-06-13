c6c90e1555b078d93c51c76995649aa27e7f7c20
Nate Ebel <n8ebel@gmail.com>
2020-06-13 13:53:10 -0700

    Add progress indicator to both CurrentForecastFragment and WeeklyForecastFragment

--- app/src/main/java/com/goobar/io/ad340/forecast/CurrentForecastFragment.kt
+++ app/src/main/java/com/goobar/io/ad340/forecast/CurrentForecastFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -30,12 +31,14 @@ class CurrentForecastFragment : Fragment() {
         val locationName = view.findViewById<TextView>(R.id.locationName)
         val tempText = view.findViewById<TextView>(R.id.tempText)
         val emptyText = view.findViewById<TextView>(R.id.emptyText)
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
 
         tempDisplaySettingManager = TempDisplaySettingManager(requireContext())
 
         // Create the observer which updates the UI in response to forecast updates
         val currentWeatherObserver = Observer<CurrentWeather> { weather ->
             emptyText.visibility = View.GONE
+            progressBar.visibility = View.GONE
             locationName.visibility = View.VISIBLE
             tempText.visibility = View.VISIBLE
 
@@ -52,7 +55,10 @@ class CurrentForecastFragment : Fragment() {
         locationRepository = LocationRepository(requireContext())
         val savedLocationObserver = Observer<Location> { savedLocation ->
             when (savedLocation) {
-                is Location.Zipcode -> forecastRepository.loadCurrentForecast(savedLocation.zipcode)
+                is Location.Zipcode -> {
+                    progressBar.visibility = View.VISIBLE
+                    forecastRepository.loadCurrentForecast(savedLocation.zipcode)
+                }
             }
         }
         locationRepository.savedLocation.observe(viewLifecycleOwner, savedLocationObserver)
--- app/src/main/java/com/goobar/io/ad340/forecast/WeeklyForecastFragment.kt
+++ app/src/main/java/com/goobar/io/ad340/forecast/WeeklyForecastFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,6 +35,8 @@ class WeeklyForecastFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_weekly_forecast, container, false)
+        val emptyText = view.findViewById<TextView>(R.id.emptyText)
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
 
         val zipcode = arguments?.getString(KEY_ZIPCODE) ?: ""
 
@@ -47,6 +51,9 @@ class WeeklyForecastFragment : Fragment() {
 
         // Create the observer which updates the UI in response to forecast updates
         val weeklyForecastObserver = Observer<WeeklyForecast> { weeklyForecast ->
+            emptyText.visibility = View.GONE
+            progressBar.visibility = View.GONE
+
             // update our list adapter
             dailyForecastAdapter.submitList(weeklyForecast.daily)
         }
@@ -60,7 +67,10 @@ class WeeklyForecastFragment : Fragment() {
         locationRepository = LocationRepository(requireContext())
         val savedLocationObserver = Observer<Location> { savedLocation ->
             when (savedLocation) {
-                is Location.Zipcode -> forecastRepository.loadWeeklyForecast(savedLocation.zipcode)
+                is Location.Zipcode -> {
+                    progressBar.visibility = View.VISIBLE
+                    forecastRepository.loadWeeklyForecast(savedLocation.zipcode)
+                }
             }
         }
         locationRepository.savedLocation.observe(viewLifecycleOwner, savedLocationObserver)
